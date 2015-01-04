11b6f216070cb6cf42ef81defdc187263d0ddc20
Jonathan Crooke <joncrooke@gmail.com>
2015-01-04 20:08:48 +0100

    New api

--- ObjcAssociatedObjectHelpers/ObjcAssociatedObjectHelpers.h
+++ ObjcAssociatedObjectHelpers/ObjcAssociatedObjectHelpers.h
@@ -65,24 +65,23 @@
 #pragma mark Assign readwrite
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define SYNTHESIZE_ASC_OBJ_ASSIGN(getterName, setterName) \
-  SYNTHESIZE_ASC_OBJ_ASSIGN_BLOCK(getterName, setterName, ^{}, ^{})
+  SYNTHESIZE_ASC_OBJ_ASSIGN_BLOCK(getterName, setterName, ^(id v){ return v; }, ^(id v){ return v; })
 
 #define SYNTHESIZE_ASC_OBJ_ASSIGN_BLOCK(getterName, setterName, getterBlock, setterBlock) \
 static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
-- (void)setterName:(id)__newValue { \
-  __block id value = __newValue; \
-  setterBlock(); \
+- (void)setterName:(id)value { \
+  value = setterBlock(value); \
   objc_AssociationPolicy policy = OBJC_ASSOCIATION_ASSIGN; \
   @synchronized(self) { \
     _OBJC_ASC_WRAP_KVO_SETTER(getterName, objc_setAssociatedObject(self, getterName##Key, value, policy)); \
   } \
 } \
 - (id) getterName { \
-  __block id value = nil; \
+  id value = nil; \
   @synchronized(self) { \
     value = objc_getAssociatedObject(self, getterName##Key); \
   }; \
-  getterBlock(); \
+  value = getterBlock(value); \
   return value; \
 }
 
@@ -90,13 +89,12 @@ static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
 #pragma mark Readwrite Object
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define SYNTHESIZE_ASC_OBJ(getterName, setterName) \
-  SYNTHESIZE_ASC_OBJ_BLOCK(getterName, setterName, ^{}, ^{})
+  SYNTHESIZE_ASC_OBJ_BLOCK(getterName, setterName, ^(id v){ return v; }, ^(id v){ return v; })
 
 #define SYNTHESIZE_ASC_OBJ_BLOCK(getterName, setterName, getterBlock, setterBlock) \
 static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
-- (void)setterName:(id)__newValue { \
-  __block id value = __newValue; \
-  setterBlock(); \
+- (void)setterName:(id)value { \
+  value = setterBlock(value); \
   objc_AssociationPolicy policy = \
   [value conformsToProtocol:@protocol(NSCopying)] ? OBJC_ASSOCIATION_COPY : OBJC_ASSOCIATION_RETAIN; \
   @synchronized(self) { \
@@ -104,11 +102,11 @@ static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
   } \
 } \
 - (id) getterName { \
-  __block id value = nil; \
+  id value = nil; \
   @synchronized(self) { \
     value = objc_getAssociatedObject(self, getterName##Key); \
   }; \
-  getterBlock(); \
+  value = getterBlock(value); \
   return value; \
 }
 
@@ -116,12 +114,12 @@ static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
 #pragma mark Lazy readonly object
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define SYNTHESIZE_ASC_OBJ_LAZY_EXP(getterName, initExpression) \
-  SYNTHESIZE_ASC_OBJ_LAZY_EXP_BLOCK(getterName, initExpression, ^{})
+  SYNTHESIZE_ASC_OBJ_LAZY_EXP_BLOCK(getterName, initExpression, ^(id v){ return v; })
 
 #define SYNTHESIZE_ASC_OBJ_LAZY_EXP_BLOCK(getterName, initExpression, block) \
 static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
 - (id)getterName { \
-  __block id value = nil; \
+  id value = nil; \
   @synchronized(self) { \
     value = objc_getAssociatedObject(self, getterName##Key); \
     if (!value) { \
@@ -129,13 +127,13 @@ static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
       objc_setAssociatedObject(self, getterName##Key, value, OBJC_ASSOCIATION_RETAIN); \
     } \
   } \
-  block(); \
+  value = block(value); \
   return value; \
 }
 
 // Use default initialiser
 #define SYNTHESIZE_ASC_OBJ_LAZY(getterName, class) \
-  SYNTHESIZE_ASC_OBJ_LAZY_EXP_BLOCK(getterName, [[class alloc] init], ^{})
+  SYNTHESIZE_ASC_OBJ_LAZY_EXP_BLOCK(getterName, [[class alloc] init], ^(id v){ return v; })
 #define SYNTHESIZE_ASC_OBJ_LAZY_BLOCK(getterName, class, block) \
   SYNTHESIZE_ASC_OBJ_LAZY_EXP_BLOCK(getterName, [[class alloc] init], block)
 
@@ -143,24 +141,23 @@ static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
 #pragma mark Primitive
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define SYNTHESIZE_ASC_PRIMITIVE(getterName, setterName, type) \
-  SYNTHESIZE_ASC_PRIMITIVE_BLOCK(getterName, setterName, type, ^{}, ^{})
+  SYNTHESIZE_ASC_PRIMITIVE_BLOCK(getterName, setterName, type, ^(type v){ return v; }, ^(type v){ return v; })
 
 #define SYNTHESIZE_ASC_PRIMITIVE_BLOCK(getterName, setterName, type, getterBlock, setterBlock) \
 static void* getterName##Key = _OBJC_ASC_QUOTE(getterName); \
-- (void)setterName:(type)__newValue { \
-  __block type value = __newValue; \
-  setterBlock(); \
+- (void)setterName:(type)value { \
+  value = setterBlock(value); \
   @synchronized(self) { \
       NSValue *nsValue = [NSValue value:&value withObjCType:@encode(type)]; \
       _OBJC_ASC_WRAP_KVO_SETTER(getterName, objc_setAssociatedObject(self, getterName##Key, nsValue, OBJC_ASSOCIATION_RETAIN)); \
   } \
 } \
 - (type) getterName { \
-  __block type value; \
+  type value; \
   memset(&value, 0, sizeof(type)); \
   @synchronized(self) { \
     [objc_getAssociatedObject(self, getterName##Key) getValue:&value]; \
   } \
-  getterBlock(); \
+  value = getterBlock(value); \
   return value; \
 }
